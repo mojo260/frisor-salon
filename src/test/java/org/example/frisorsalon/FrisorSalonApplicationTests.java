c0245aebbd48a914b2d0a899eb5f26a45629e44a
mojo260 <142881889+mojo260@users.noreply.github.com>
2024-11-26 11:24:48 +0100

    FIRST

---
+++ src/test/java/org/example/frisorsalon/FrisorSalonApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.frisorsalon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrisorSalonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
